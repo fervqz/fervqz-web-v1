20ef97b7ac5d261f977e1b122a5fbb44f6549a2b
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-14 03:51:48 +0200

    Adding constans

---
+++ src/constants/skills.ts
@@ -0,0 +1,16 @@
+const SKILLS = [
+    { slug: "ts", lalbel: "TypeScript" },
+    { slug: "js", lalbel: "JavaScript" },
+    { slug: "html", lalbel: "HTML" },
+    { slug: "css", lalbel: "CSS" },
+    { slug: "react", lalbel: "React" },
+    { slug: "astro", lalbel: "Astro" },
+    { slug: "angular", lalbel: "Angular" },
+    { slug: "nextjs", lalbel: "Next.js" },
+    { slug: "cypress", lalbel: "Cypress" },
+    { slug: "aws", lalbel: "AWS" },
+    { slug: "tailwind", lalbel: "Tailwind" },
+    { slug: "git", lalbel: "Git" },
+  ];
+
+  export default SKILLS;
