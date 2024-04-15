733f85d139d5d17acd6d722903d0b4e88214e28d
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-15 15:20:26 +0200

    changing icon library

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://localhost:4321',
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
