c52f432863cb006397d9e49403212c21648945c3
Fernando Vásquez <f.vasquez@softec.cloud>
2024-04-23 22:32:39 +0200

    Changing experience layout and adding images

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -3,6 +3,7 @@ const EXPERIENCE = [
       role: "Junior Full-Stack Developer",
       company: "FUNIBER",
       date: "Nov 2016 — Jul 2020",
+      img: "/img/logo-funiber.webp",
       activities: [
         "<b>Reduced by 99.8% the application's time (from weeks to minutes)</b> for university certificates requests by implementing a responsive progressive web application with a bespoke business logic.",
         "<b>Decreased, by 90%, errors caused by human intervention</b> on university certificates requests.",
@@ -13,6 +14,7 @@ const EXPERIENCE = [
       role: "Full-Stack Developer",
       company: "Diatomea Technology",
       date: "Jul 2020 — Mar 2022",
+      img: "/img/logo-diatomea.webp",
       activities: [
         "<b>Reduced by 30% delivery times</b> by developing a serverless web application with AWS infrastructure for managing delivery routes and packages vehicles storage optimization for a supermarket chain.",
         "Developed a mobile app for covid-19 tests results management, <b>boosting results registration by 300%</b> on a Public Health System.",
@@ -23,6 +25,7 @@ const EXPERIENCE = [
       role: "Full-Stack Developer",
       company: "Softec Cloud",
       date: "Mar 2022 — Jan 2024",
+      img: "/img/logo-softec.webp",
       activities: [
         "Developed  <b>over +35 NextJS/AngularJS websites with an average traffic of +50k users per month</b>.",
         "Migrated the main contracting process for hiring electricity and gas services for a large company, from PHP to test-driven AngularJS web app, <b>resulting in a 10% increase in the annual revenue</b> for the business line.",
