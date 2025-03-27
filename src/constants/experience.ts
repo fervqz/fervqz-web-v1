95f77a9e82d3a1d04299dd92a791cf9a2fb3b3ab
Fernando Vásquez <fernando.vasquez.9779@gmail.com>
2025-03-27 16:37:29 -0600

    hotfix: update roles

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,48 +1,48 @@
 const EXPERIENCE = [
-    {
-      role: "Junior Software Engineer",
-      company: "FUNIBER",
-      date: "Nov 2016 — Jul 2018",
-      img: "/img/logo-funiber.webp",
-      activities: [
-        "<b>Reduced by 99.8% the application's time (from weeks to minutes)</b> for university certificates requests by implementing a responsive progressive web application with a bespoke business logic.",
-        "<b>Decreased, by 90%, errors caused by human intervention</b> on university certificates requests.",
-        "Developed a bookings management and issues tracker web application for a Students Residence, <b>decreasing by 60% of incorrect room assignments, improved issues solving 200% faster, and automation reports generation</b>.",
-      ],
-    },
-    {
-      role: "Software Engineer",
-      company: "Diatomea Technology",
-      date: "Jul 2018 — Mar 2022",
-      img: "/img/logo-diatomea.webp",
-      activities: [
-        "<b>Reduced by 30% delivery times</b> by developing a serverless web application with AWS infrastructure for managing delivery routes and packages vehicles storage optimization for a supermarket chain.",
-        "Developed a mobile app for covid-19 tests results management, <b>boosting results registration by 300%</b> on a Public Health System.",
-        "Migrated a KPIs monitoring web app for an industrial wire manufacturing company, built with ReactJS (SPA) and migrated to NextJS (SSR) <b>improving its performance by 50%</b> on thin client operators devices.",
-      ],
-    },
-    {
-      role: "Software Engineer",
-      company: "Softec Cloud",
-      date: "Mar 2022 — Jan 2024",
-      img: "/img/logo-softec.webp",
-      activities: [
-        "Developed  <b>over +35 NextJS/AngularJS websites with an average traffic of +50k users per month</b>.",
-        "Migrated the main contracting process for hiring electricity and gas services for a large company, from PHP to test-driven AngularJS web app, <b>resulting in a 10% increase in the annual revenue</b> for the business line.",
-        "Designed and developed a components library with ReactJS and TailwindCSS for unifying all design systems of each company's web application.",
-      ],
-    },
-    {
-      role: "Frontend Engineer",
-      company: "Knack",
-      date: "Jul 2024 — Today",
-      img: "/img/logo-knack.svg",
-      activities: [
-        "Migrated the company's core product to its latest version, adhering to <b>industry-leading standards</b> and ensuring a seamless user experience.",
-	      "Designed and implemented a <b>reusable components system</b> to streamline development across multiple projects, improving efficiency and consistency.",
-	      "Contributed to code reviews, ensuring adherence to <b>best practices</b> and maintaining codebase quality.",
-      ],
-    },
-  ];
+  {
+    role: "Junior Full-Stack Developer",
+    company: "FUNIBER",
+    date: "Nov 2016 — Jul 2018",
+    img: "/img/logo-funiber.webp",
+    activities: [
+      "<b>Reduced by 99.8% the application's time (from weeks to minutes)</b> for university certificates requests by implementing a responsive progressive web application with a bespoke business logic.",
+      "<b>Decreased, by 90%, errors caused by human intervention</b> on university certificates requests.",
+      "Developed a bookings management and issues tracker web application for a Students Residence, <b>decreasing by 60% of incorrect room assignments, improved issues solving 200% faster, and automation reports generation</b>.",
+    ],
+  },
+  {
+    role: "Full-Stack Developer",
+    company: "Diatomea Technology",
+    date: "Jul 2018 — Mar 2022",
+    img: "/img/logo-diatomea.webp",
+    activities: [
+      "<b>Reduced by 30% delivery times</b> by developing a serverless web application with AWS infrastructure for managing delivery routes and packages vehicles storage optimization for a supermarket chain.",
+      "Developed a mobile app for covid-19 tests results management, <b>boosting results registration by 300%</b> on a Public Health System.",
+      "Migrated a KPIs monitoring web app for an industrial wire manufacturing company, built with ReactJS (SPA) and migrated to NextJS (SSR) <b>improving its performance by 50%</b> on thin client operators devices.",
+    ],
+  },
+  {
+    role: "Front-End Developer",
+    company: "Softec Cloud",
+    date: "Mar 2022 — Jan 2024",
+    img: "/img/logo-softec.webp",
+    activities: [
+      "Developed  <b>over +35 NextJS/AngularJS websites with an average traffic of +50k users per month</b>.",
+      "Migrated the main contracting process for hiring electricity and gas services for a large company, from PHP to test-driven AngularJS web app, <b>resulting in a 10% increase in the annual revenue</b> for the business line.",
+      "Designed and developed a components library with ReactJS and TailwindCSS for unifying all design systems of each company's web application.",
+    ],
+  },
+  {
+    role: "Front-End Developer",
+    company: "Knack",
+    date: "Jul 2024 — Today",
+    img: "/img/logo-knack.svg",
+    activities: [
+      "Migrated the company's core product to its latest version, adhering to <b>industry-leading standards</b> and ensuring a seamless user experience.",
+      "Designed and implemented a <b>reusable components system</b> to streamline development across multiple projects, improving efficiency and consistency.",
+      "Contributed to code reviews, ensuring adherence to <b>best practices</b> and maintaining codebase quality.",
+    ],
+  },
+];
 
-  export default EXPERIENCE;
+export default EXPERIENCE;
